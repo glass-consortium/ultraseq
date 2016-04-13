acba864b15806f9b0306364785e4c14fe3d285d7
sseth <me@sahilseth.com>
2016-04-13 18:18:15 -0500

    changes to bam_mutect

---
+++ pipelines/bam_mutect.R
@@ -0,0 +1,127 @@
+
+library(ultraseq)
+
+
+# get path to this script
+# addional functionality to R
+#script_path = funr::get_script_path()
+#opts_flow$load(file.path(script_path, "ultraseq.conf"))
+
+help_text_flowr_run = "
+Optional (used by flowr::run):
+def             flow definition file
+platform        platform, to over-ride flow definition file
+conf            tab delimited configuration file
+wd              base directory used for this run
+execute         logical, controlling submission of this flow to the cluster
+                OR running on the local platform
+use 'flowr run -h' for more details on additional optional arguments.
+"
+
+# roxygen2:::parse_file("../pipelines/bam_mutect.R", as.environment("package:ultraseq"))
+
+
+#' Title
+#'
+#' @param tumor_bam
+#' @param normal_bam
+#'
+#' @return
+#' @export
+#'
+#' @examples
+bam_mutect <- function(tumor_bam, 
+                       normal_bam, 
+                       samplename_t,
+                       samplename_r,# usually tumor___normal
+                       out_prefix
+){
+  
+  help_text = "
+
+Usage:
+flowr run bam_mutect tumor_bam=tumor.bam normal_bam=normal.bam out_prefix=tumor_normal platform=local execute=FALSE
+
+Required:
+tumor_bam       tumor bam file
+normal_bam      normal bam file
+out_prefix     output paired name
+"
+  
+  help_text = paste0(help_text, help_text_flowr_run)
+  
+  check_args()
+  
+  if(missing(tumor_bam))
+    stop(help_text)
+  
+  # bam file names depend on the sample names we supply
+  #fq_pre = fetch_pipes("fastq_preprocess")
+  paired_name = paste0(samplename_t, "___", samplename_r)
+  #debug(preprocess)
+  preproc_tum = preprocess(bam = tumor_bam, 
+                           samplename = samplename_t,
+                           split_by_chr = TRUE)
+  
+  preproc_ref = preprocess(bam = tumor_bam, 
+                           samplename = samplename_r,
+                           split_by_chr = TRUE)
+  
+  # add suffix to jobnme
+  preproc_tum$flowmat$jobname = paste0(preproc_tum$flowmat$jobname, "_t")
+  preproc_ref$flowmat$jobname = paste0(preproc_ref$flowmat$jobname, "_r")
+  
+  # -- run mutect on each of the smaller chrs
+  #print(preproc_tum$outfiles$recalibed_bam)
+  
+  mut = mutect(tumor_bam = preproc_tum$outfiles,
+               normal_bam = preproc_ref$outfiles,
+               out_prefix = out_prefix,
+               is_merged = FALSE, # input files are by chr
+               split_by_chr = TRUE,
+               samplename = paired_name)
+  
+  ## generate a bigger flowdef with all the elements
+  flowmat = rbind(preproc_tum$flowmat, preproc_ref$flowmat, mut$flowmat)
+  
+  ## since the three modules are to be submitted as a single pipeline
+  ## we will put the same sample name throughout
+  flowmat$samplename = paired_name
+  
+  #def = to_flowdef(flowmat)
+  #write_sheet(def, "analysis/fastq_mutect.def")
+  
+  return(list(flowmat = flowmat, outfiles = mut$outfiles))
+  
+  
+}
+
+
+#debug(bam_mutect)
+
+
+
+# --- testing:
+
+if(FALSE){
+  
+  source('../pipelines/bam_mutect.R')
+  devtools::load_all()
+  #debug(bam_mutect)
+  #debug(mutect);
+  #debug(paired_bam_set)
+  #debug(preprocess)
+  out = bam_mutect(
+    tumor_bam="inst/testdata/bams/tumor.bam", 
+    normal_bam="inst/testdata/bams/normal.bam",
+    paired_name = "tumor_normal", out_prefix="tumor_normal")
+  
+  run("../pipelines/bam_mutect", tumor_bam="tumor.bam", 
+      normal_bam="normal.bam",
+      paired_name = "tumor_normal",
+      execute = FALSE,
+      out_prefix="tumor_normal")
+  
+  
+  
+}
